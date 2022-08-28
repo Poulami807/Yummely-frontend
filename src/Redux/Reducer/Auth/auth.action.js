4634ba48580c6cb1b1c4fdc9580c4778bd70b03a
Poulami807 <poulamiganguly10@gmail.com>
2022-08-29 01:49:08 +0530

    _redirects added

--- src/Redux/Reducer/Auth/auth.action.js
+++ src/Redux/Reducer/Auth/auth.action.js
@@ -14,7 +14,7 @@ export const signIn = (userData) => async (dispatch) => {
       data: { credentials: userData },
     });
 
-    window.location.href = "http://localhost:3000/delivery";
+    window.location.href = "https://yummely.netlify.app/delivery";
 
     localStorage.setItem(
       "yummelyUser",
@@ -36,7 +36,7 @@ export const signUp = (userData) => async (dispatch) => {
       data: { credentials: userData },
     });
 
-    window.location.href = "http://localhost:3000/delivery";
+    window.location.href = "hhttps://yummely.netlify.app/delivery";
 
     localStorage.setItem(
       "yummelyUser",
@@ -55,7 +55,7 @@ export const googleAuth = (token) => async (dispatch) => {
     localStorage.setItem("yummelyUser", JSON.stringify({ token }));
 
     dispatch({ type: GOOGLE_AUTH, payload: {} });
-    window.location.href = "http://localhost:3000/delivery";
+    window.location.href = "https://yummely.netlify.app/delivery";
     
   } catch (error) {
     return dispatch({ type: "ERROR", payload: error });
@@ -66,7 +66,7 @@ export const signOut = () => async (dispatch) => {
   try {
     localStorage.removeItem("yummelyUser");
     clearUser();
-    window.location.href = "http://localhost:3000/delivery";
+    window.location.href = "https://yummely.netlify.app/delivery";
 
     return dispatch({ type: SIGN_OUT, payload: {} });
   } catch (error) {
